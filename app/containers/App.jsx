c1b69246495108160e4fd38d518aa2e8f2eb834d
Huayun Liu <huayun.liu@sap.com>
2017-04-12 19:17:57 +0800

    Add react-router.

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+} from 'react-router-dom'
 
 import { add } from 'ACTIONS/count'
 
@@ -7,13 +12,27 @@ class App extends Component {
     render() {
         const { dispatch } = this.props
         return (
+        <Router>
             <div className='app'>
                 { this.props.count }
                 <button onClick={() => dispatch(add())}>Add</button>
+                <Link to='/home'>Home</Link>
+                <Link to='/page'>Page</Link>
+
+                {
+                    true
+                }
+                <hr />
+                ''
+                <Route path='/home' render={()=> <button>Home</button>} />
+                <hr />
+                <Route path='/page' render={()=> <button>Page</button>} />
             </div>
+        </Router>
         )
     }
 }
+
 function select(state) {
     return {
         count: state.count
